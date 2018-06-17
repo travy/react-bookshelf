36da82ff65fe3688d38c79cb5474e860ba61ca50
Travis Anthony Torres <travis@travistorres.com>
2018-06-17 14:25:12 -0400

    feat-5: Update Grids
    Will update the bookshelf dashboard whenever the user moves a book to a
    new shelf.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,7 +5,7 @@ const BookShelf = props => (
     <div className='bookshelf'>
         <h2 className='bookshelf-title'>{props.title}</h2>
         <div className='bookshelf-books'>
-            <BookGrid books={props.books} />
+            <BookGrid books={props.books} {...props} />
         </div>
     </div>
 )
